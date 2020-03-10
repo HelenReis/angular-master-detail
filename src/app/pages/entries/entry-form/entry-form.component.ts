adc2560fd9efec6e3afad7408af6f108f187de9f
Helen Reis <helen_reis@lifesys.com.br>
2020-03-10 17:59:15 -0300

    fixing bugs

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -167,13 +167,14 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   private updateEntry() {
     const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
 
-    this.entryService.update(entry);
+    this.entryService.update(entry).subscribe(
+      entry => this.actionsForSuccess(entry),
+      error => this.actionsForErrors(error)
+    );
   }
 
   private actionsForSuccess(entry: Entry) {
     alert("Solicitação processada com sucesso");
-
-    debugger;
 
     this.router
       .navigateByUrl("entries", { skipLocationChange: true })
